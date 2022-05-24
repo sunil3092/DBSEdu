97ff0fefb4359e607ed7f4cae112fccc43693e84
Sunil <sunilg3011992@gmail.com>
2022-05-24 12:09:15 +0200

    Working on Agent

--- Sem 2/Reinforcement Learning/Experimentations/agent.py
+++ Sem 2/Reinforcement Learning/Experimentations/agent.py
@@ -1,7 +1,12 @@
 # conda env export > environment.yml
 # conda env create -f environment.yml
 # conda env update -f environment.yml
-import pytorch
+import torch
 import random
 import numpy as np
-from snake_game import SnakeGameAI
+from collections import deque
+from snake_game import SnakeGameAI, Direction, Point
+
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
