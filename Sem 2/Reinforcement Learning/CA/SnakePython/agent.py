31c6f66159c2a5a1f7a4ea06ae7efa0c1bcb12d7
Sunil <sunilg3011992@gmail.com>
2022-07-06 11:28:06 +0200

    Working on train_step function

--- Sem 2/Reinforcement Learning/CA/SnakePython/agent.py
+++ Sem 2/Reinforcement Learning/CA/SnakePython/agent.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
+from model import Liner_Qnet, Q_Trainer
 
 
 MAX_MEMORY = 100_000
@@ -15,11 +16,11 @@ class Agent:
     def __init__(self):
         self.n_games = 0
         self.epsilon = 0  # Param to control randomness
-        self.gamma = 0  # Discount Rate
+        self.gamma = 0.9  # Discount Rate
         self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
-        self.model = None  # TODO
-        self.trainer = None  # TODO
-        # TODO: model, trainer
+        self.model = Liner_Qnet(11, 256, 3)
+        self.trainer = Q_Trainer(
+            self.model, lr=LEARNING_RATE, gamma=self.gamma)
 
     def get_state(self, game):
         head = game.snake[0]
@@ -68,16 +69,31 @@ class Agent:
         return np.array(state, dtype=int)
 
     def remember(self, state, action, reward, next_state, done):
-        self.memory.append(state, action, reward, next_state, done)
+        self.memory.append((state, action, reward, next_state, done))
 
     def train_long_memory(self):
-        pass
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
+        else:
+            mini_sample = self.memory
+        states, actions, rewards, next_states, dones = zip(*mini_sample)
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
 
     def train_short_memory(self, state, action, reward, next_state, done):
         self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        pass
+        self.epsilon = 80 - self.n_games
+        final_move = [0, 0, 0]
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+        return final_move
 
 
 def train():
@@ -114,7 +130,7 @@ def train():
 
             if score > record:
                 record = score
-                # agent.model.save()
+                agent.model.save()
 
             print('Game:', agent.n_games, 'Score:', score, "Record:", record)
 
